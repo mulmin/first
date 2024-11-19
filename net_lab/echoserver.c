8049a7480a39168bf0823859bc947550a5f77289
Eunji Lee <eunjicious@gmail.com>
2024-11-19 14:51:25 +0900

    net_lab added

---
+++ net_lab/echoserver.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+#define LISTENQ 1024
+#define MAXLINE 1024
+
+int main(int argc, char **argv) 
+{
+	return 0;
+}
